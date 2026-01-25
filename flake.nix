1ac2155e86e704a8778371ab0a37444c67e1d1e5
Simon R. - fantomitechno <me@fantomitechno.dev>
2026-01-25 16:48:59 +0100

    ✨ Add MSI computer

--- flake.nix
+++ flake.nix
@@ -28,21 +28,39 @@
 
     in
     {
-      nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = { inherit globals; };
-        modules = [
-          ./hosts/laptop/configuration.nix
-          ./hosts/common
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit globals; };
-            home-manager.users.${globals.UserName} = import ./home/home.nix;
-          }
-          minegrub-theme.nixosModules.default
-        ];
+      nixosConfigurations = {
+        laptop = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit globals; };
+          modules = [
+            ./hosts/laptop/configuration.nix
+            ./hosts/common
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = { inherit globals; };
+              home-manager.users.${globals.UserName} = import ./home/home.nix;
+            }
+            minegrub-theme.nixosModules.default
+          ];
+        };
+        msi = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit globals; };
+          modules = [
+            ./hosts/msi/configuration.nix
+            ./hosts/common
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = { inherit globals; };
+              home-manager.users.${globals.UserName} = import ./home/home.nix;
+            }
+            minegrub-theme.nixosModules.default
+          ];
+        };
       };
     };
 }
---
+++ hosts/msi/configuration.nix
@@ -0,0 +1,49 @@
+{
+  config,
+  pkgs,
+  globals,
+  inputs,
+  ...
+}:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ./packages.nix
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  # Bootloader
+  boot.loader = {
+    systemd-boot.enable = true;
+    grub.enable = false;
+    grub.device = "/dev/nvme0n1p1";
+    grub.minegrub-theme = {
+      enable = true;
+      splash = "NixOS for the win!";
+    };
+    efi.canTouchEfiVariables = true;
+  };
+  boot.kernelPackages = pkgs.linuxPackages;
+
+  services.xserver.xkb = {
+    layout = "fr";
+    variant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "fr";
+
+  environment.variables = {
+    LD_LIBRARY_PATH = "/run/opengl-driver/lib";
+    ELECTRON_OZONE_PLATFORM_HINT = "wayland";
+    OZONE_PLATFORM = "wayland";
+    GDK_BACKEND = "wayland";
+  };
+
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # networking.firewall.enable = false;
+  system.stateVersion = "25.11";
+}
