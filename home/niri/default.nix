fc0bc41304acb849c2ffb3982cb202b865ed810f
Simon R. - fantomitechno <me@fantomitechno.dev>
2026-02-10 23:14:47 +0100

    ♻️ Rewrite niri's logic

--- home/niri/default.nix
+++ home/niri/default.nix
@@ -1,10 +1,14 @@
 { config, type, ... }:
 let
   dotfileFolder = "${config.home.homeDirectory}/dotfiles";
-  niriFolder = "${dotfileFolder}/home/niri/${type}";
+  niriFolder = "${dotfileFolder}/home/niri";
 in
 {
-  xdg.configFile."niri".source = config.lib.file.mkOutOfStoreSymlink "${niriFolder}/config";
+  xdg.configFile."niri/config.kdl".source =
+    config.lib.file.mkOutOfStoreSymlink "${niriFolder}/config/${type}.kdl";
+  xdg.configFile."niri/common".source =
+    config.lib.file.mkOutOfStoreSymlink "${niriFolder}/config/common";
+
   xdg.configFile."swaylock/config".source =
     config.lib.file.mkOutOfStoreSymlink "${niriFolder}/swaylock.conf";
   home.file.".local/bin/nws.sh".source = config.lib.file.mkOutOfStoreSymlink "${niriFolder}/nws.sh";
