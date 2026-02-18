88134f2688c26306baa0537384fce0d585f1eb08
Simon R. - fantomitechno <me@fantomitechno.dev>
2026-02-18 16:07:17 +0100

    ✨ Things

--- home/zsh.nix
+++ home/zsh.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.zsh = {
@@ -31,5 +31,18 @@
         "history-substring-search"
       ];
     };
+
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.8.0";
+          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+        };
+      }
+    ];
   };
 }
