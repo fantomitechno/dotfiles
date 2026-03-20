ee5f43afad98f04ce1b568dfd9eddf9aa7e68801
Simon R. - fantomitechno <me@fantomitechno.dev>
2026-03-20 12:24:48 +0100

    ⚡️ Better battery notification

--- hosts/common/battery-notify.nix
+++ hosts/common/battery-notify.nix
@@ -9,16 +9,17 @@
     serviceConfig = {
       Type = "simple";
       ExecStart = pkgs.writeShellScript "battery-low-notification" ''
-        if (( 10 >= $(${pkgs.lib.getExe pkgs.acpi} -b | head -n 1 | ${pkgs.lib.getExe pkgs.ripgrep} -o "\d+%" | ${pkgs.lib.getExe pkgs.ripgrep} -o "\d+")));
-        then ${pkgs.lib.getExe pkgs.pkgs.libnotify} --urgency=critical "low battery" "$(${pkgs.lib.getExe pkgs.acpi} -b | head -n 1 | ${pkgs.lib.getExe pkgs.ripgrep} -o "\d+%")";
-        else echo; fi;
+        BATTERY=$(${pkgs.lib.getExe pkgs.acpi} -b | head -n 1)
+        LEVEL=$(echo "$BATTERY" | ${pkgs.lib.getExe pkgs.ripgrep} -o "\d+%" | ${pkgs.lib.getExe pkgs.ripgrep} -o "\d+")
+        if echo "$BATTERY" | ${pkgs.lib.getExe pkgs.ripgrep} -q "Discharging" && (( 10 >= LEVEL )); then
+          ${pkgs.lib.getExe pkgs.libnotify} --urgency=critical "low battery" "$LEVEL%"
+        fi
       '';
     };
   };
   systemd.user.timers."battery-low" = {
     wantedBy = [ "timers.target" ];
     timerConfig = {
-      # Every Minute
       OnCalendar = "*-*-* *:*:00";
       Unit = "battery-low.service";
     };
