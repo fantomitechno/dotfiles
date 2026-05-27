9f792ad093024fd379e964e572ac3e16ab9d248e
Simon R. - fantomitechno <me@fantomitechno.dev>
2026-05-27 21:33:06 +0200

    Revert "🐛 Change to target if more batteries are there"
    This reverts commit b89b08416baddf063872e074088ee07d87f67a13.

--- hosts/common/battery-notify.nix
+++ hosts/common/battery-notify.nix
@@ -9,11 +9,9 @@
     serviceConfig = {
       Type = "simple";
       ExecStart = pkgs.writeShellScript "battery-low-notification" ''
-        LAST_BATTERY=$(${pkgs.lib.getExe pkgs.acpi} -b | tail -n 1)
-        LEVEL=$(echo "$LAST_BATTERY" | ${pkgs.lib.getExe pkgs.ripgrep} -o "\d+%" | ${pkgs.lib.getExe pkgs.ripgrep} -o "\d+")
-        ALL_DISCHARGING=$(${pkgs.lib.getExe pkgs.acpi} -b | ${pkgs.lib.getExe pkgs.ripgrep} -c "Discharging")
-        TOTAL=$(${pkgs.lib.getExe pkgs.acpi} -b | ${pkgs.lib.getExe pkgs.coreutils}/bin/wc -l)
-        if [[ "$ALL_DISCHARGING" == "$TOTAL" ]] && (( 10 >= LEVEL )); then
+        BATTERY=$(${pkgs.lib.getExe pkgs.acpi} -b | head -n 1)
+        LEVEL=$(echo "$BATTERY" | ${pkgs.lib.getExe pkgs.ripgrep} -o "\d+%" | ${pkgs.lib.getExe pkgs.ripgrep} -o "\d+")
+        if echo "$BATTERY" | ${pkgs.lib.getExe pkgs.ripgrep} -q "Discharging" && (( 10 >= LEVEL )); then
           ${pkgs.lib.getExe pkgs.libnotify} --urgency=critical "low battery" "$LEVEL%"
         fi
       '';
