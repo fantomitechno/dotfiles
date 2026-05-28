6672ef05f00ca672057ba001a75f7cf00413a187
Simon R. - fantomitechno <me@fantomitechno.dev>
2026-05-28 09:07:09 +0200

    🐛 Real fix of the battery script

--- hosts/common/battery-notify.nix
+++ hosts/common/battery-notify.nix
@@ -9,10 +9,23 @@
     serviceConfig = {
       Type = "simple";
       ExecStart = pkgs.writeShellScript "battery-low-notification" ''
-        BATTERY=$(${pkgs.lib.getExe pkgs.acpi} -b | head -n 1)
-        LEVEL=$(echo "$BATTERY" | ${pkgs.lib.getExe pkgs.ripgrep} -o "\d+%" | ${pkgs.lib.getExe pkgs.ripgrep} -o "\d+")
+        BATTERIES=$(${pkgs.lib.getExe pkgs.acpi} -b)
+        LEVEL=200
+        N=$(echo "$BATTERIES" | wc -l)
+        for ((i = 1 ; i <= N ; i++)); do
+          BATTERY=$(echo "$BATTERIES" | sed -n "''${i}p")
+          if echo "$BATTERY" | grep -q "rate information unavailable"; then
+            echo "Skipping: $BATTERY"
+          else
+            NEW_LEVEL=$(echo "$BATTERY" | ${pkgs.lib.getExe pkgs.ripgrep} -o "\d+%" | ${pkgs.lib.getExe pkgs.ripgrep} -o "\d+")
+            if ((NEW_LEVEL < LEVEL)); then
+              LEVEL=$NEW_LEVEL
+            fi
+          fi 
+        done
+        echo "$LEVEL"
         if echo "$BATTERY" | ${pkgs.lib.getExe pkgs.ripgrep} -q "Discharging" && (( 10 >= LEVEL )); then
-          ${pkgs.lib.getExe pkgs.libnotify} --urgency=critical "low battery" "$LEVEL%"
+        ${pkgs.lib.getExe pkgs.libnotify} --urgency=critical "low battery" "$LEVEL%"
         fi
       '';
     };
