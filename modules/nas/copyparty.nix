f04214ad851572f0801940b6fe8e75aa643689c5
Simon R. - fantomitechno <me@fantomitechno.dev>
2026-06-26 15:18:29 +0200

    🏗️ Change copyparty from an input to a tarball

--- modules/nas/copyparty.nix
+++ modules/nas/copyparty.nix
@@ -1,19 +1,28 @@
 {
   dotfileFolder,
-  flakes,
   ...
 }:
-
+let
+  copyparty = fetchTarball {
+    url = "https://github.com/9001/copyparty/releases/download/v1.20.16/copyparty-1.20.16.tar.gz";
+    sha256 = "04ym88n720svqmp8ffmsymav1mn30s6ikgfnqy42nh11205v22s1";
+  };
+in
 {
+  # load the copyparty NixOS module
+  imports = [ "${copyparty}/contrib/nixos/modules/copyparty.nix" ];
+
+  # add the copyparty overlay to expose the package to the module
+  nixpkgs.overlays = [ (import "${copyparty}/contrib/package/nix/overlay.nix") ];
+
   services.copyparty = {
     enable = true;
     user = "copyparty";
     group = "copyparty";
     settings = {
-      i = "172.16.20.10";
+      i = "127.0.0.1";
       p = [
         3210
-        3211
       ];
       no-reload = true;
       ignored-flag = false;
@@ -27,7 +36,7 @@
 
     volumes = {
       "/public" = {
-        # path = ""
+        path = "/mnt/data/public";
 
         access = {
           r = "*";
@@ -39,9 +48,5 @@
         };
       };
     };
-  };
-
-  nixpkgs = {
-    overlays = [ flakes.inputs.copyparty.overlays.default ];
   };
 }
