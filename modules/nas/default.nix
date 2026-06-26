a059476fc7f4b4ee6a77de313e6902efc6485999
Simon R. - fantomitechno <me@fantomitechno.dev>
2026-06-26 15:23:12 +0200

    ✨ firewalling my firewall

--- modules/nas/default.nix
+++ modules/nas/default.nix
@@ -13,4 +13,25 @@
     #
     # '';
   };
+
+  networking.firewall = {
+    enable = true;
+    # for NFSv3; view with `rpcinfo -p`
+    allowedTCPPorts = [
+      111
+      2049
+      4000
+      4001
+      4002
+      20048
+    ];
+    allowedUDPPorts = [
+      111
+      2049
+      4000
+      4001
+      4002
+      20048
+    ];
+  };
 }
