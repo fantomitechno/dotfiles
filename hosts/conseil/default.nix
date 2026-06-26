1a5cf111a8c803481ab222c3596b5561c689bf00
Simon R. - fantomitechno <me@fantomitechno.dev>
2026-06-26 09:52:18 +0200

    🚚 move nas config back on conseil host

--- hosts/conseil/default.nix
+++ hosts/conseil/default.nix
@@ -1,5 +1,6 @@
 {
   pkgs,
+  dotfileFolder,
   ...
 }:
 
@@ -7,10 +8,7 @@
   imports = [
     ./hardware-configuration.nix
     ../server.nix
-  ]
-  ++ builtins.map (i: ../../modules/${i}) ([
-    "nas"
-  ]);
+  ];
 
   # Bootloader
   boot = {
@@ -19,6 +17,72 @@
       efi.canTouchEfiVariables = true;
     };
     kernelPackages = pkgs.linuxPackages;
+  };
+
+  services = {
+    nfs.server = {
+      enable = true;
+      statdPort = 4000;
+      lockdPort = 4001;
+      mountdPort = 4002;
+      # exports = ''
+      #
+      # '';
+    };
+
+    copyparty = {
+      enable = true;
+      user = "copyparty";
+      group = "copyparty";
+      settings = {
+        i = "172.16.20.10";
+        p = [
+          3210
+          3211
+        ];
+        no-reload = true;
+        ignored-flag = false;
+      };
+
+      accounts = {
+        fantomitechno = {
+          passwordFile = "${dotfileFolder}/hosts/conseil/pswd/copyparty-fantom";
+        };
+      };
+
+      volumes = {
+        "/public" = {
+          # path = ""
+
+          access = {
+            r = "*";
+            rw = [ "fantomitechno" ];
+          };
+
+          flags = {
+            scan = 60;
+          };
+        };
+      };
+    };
+
+    garage = {
+      enable = true;
+
+      settings = {
+        metadata_dir = "/var/lib/garage/meta";
+        data_dir = "/var/lib/garage/data";
+
+        s3_api = {
+          api_bind_addr = "[::]:3900";
+          s3_region = "garage";
+        };
+
+        s3_web = {
+          bind_addr = "[::]:3902";
+        };
+      };
+    };
   };
 
   system.stateVersion = "26.05";
