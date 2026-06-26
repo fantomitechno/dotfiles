41d18a861fd772d97dbd68a84312c7492ccb19e6
Simon R. - fantomitechno <me@fantomitechno.dev>
2026-06-26 15:19:10 +0200

    ✨ Matrix

---
+++ modules/matrix.nix
@@ -0,0 +1,23 @@
+{ ... }: {
+  services.matrix-conduit = {
+    enable = true;
+
+    settings = {
+      global = {
+        allow_registration = false;
+        allow_federation = true;
+
+        database_backend = "rocksdb";
+
+        server_name = "matrix.fantomitechno.dev";
+        trusted_servers = [
+          "matrix.org"
+          "faimaison.net"
+        ];
+
+        address = "127.0.0.1";
+        port = 6167;
+      };
+    };
+  };
+}
