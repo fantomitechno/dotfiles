1b87d3c71269a3088b4ec61020e07ec5590aa56d
Simon R. - fantomitechno <me@fantomitechno.dev>
2026-08-28 22:04:31 +0200

    everest work

--- modules/modding.nix
+++ modules/modding.nix
@@ -6,6 +6,14 @@
     avalonia-ilspy
 
     dotnet-sdk_8
+    dotnet-sdk_9
     dotnet-runtime_8
+    dotnet-runtime_9
+    fna3d
+    mono
   ];
+
+  environment.sessionVariables = {
+    DOTNET_SYSTEM_GLOBALIZATION_INVARIANT = 1;
+  };
 }
